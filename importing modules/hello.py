6721519b2d73ff0111eb2950a5d96d53e0dfba0a
Seungyoun Lee <yulialee217@gmail.com>
2026-05-18 19:18:59 -0400

    Add notes

---
+++ importing modules/hello.py
@@ -0,0 +1,4 @@
+import hi
+
+hi.greetings("Yulia")
+print(hi.person1["name"])
---
+++ importing modules/hi.py
@@ -0,0 +1,8 @@
+def greetings(name):
+    print("Good morning", name)
+
+person1={
+    "name": "Seungyoun",
+    "age": 25,
+    "hobby":"tennis"
+}
